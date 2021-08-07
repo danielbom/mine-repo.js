4bf904126e02dfaae006b59dcbc4a230b7a4ce44
danielbom <daniel.rodrigues.45@hotmail.com>
2021-08-07 00:37:47 -0300

    Refactoring computePaginated()

--- src/with-functions/operations/computePaginated.js
+++ src/with-functions/operations/computePaginated.js
@@ -3,10 +3,10 @@ const Promise = require("bluebird");
 async function computePaginated({ getPaginated, mapper, concurrency }) {
   let page = 0;
   while (true) {
-    const pullRequests = await getPaginated({ page });
-    if (pullRequests.length === 0) break;
+    const items = await getPaginated({ page });
+    if (items.length === 0) break;
 
-    await Promise.map(pullRequests, mapper, { concurrency });
+    await Promise.map(items, mapper, { concurrency });
     page++;
   }
 }
