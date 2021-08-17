4507b3346c54fb20c240c7861a453e992ad8f372
danielbom <daniel.rodrigues.45@hotmail.com>
2021-08-17 07:19:17 -0300

    Add rotateKeys() on fetch()

--- src/with-functions/fetch.js
+++ src/with-functions/fetch.js
@@ -1,6 +1,5 @@
 const api = require("../apis/github");
 const logger = require("./logger");
-const sleep = require("./sleep");
 const catchSafeErrors = require("./catchSafeErrors");
 const counterFetch = require("./counterFetch");
 const config = require("../config");
@@ -8,22 +7,40 @@ const config = require("../config");
 const keys = config.GITHUB_APIKEYS;
 const keysCount = config.GITHUB_APIKEYS.length;
 
-function setApiKeyHeader(api, index) {
-  const githubApi = keys[index % keysCount];
-  api.defaults.headers.Authorization = `Bearer ${githubApi}`;
-}
+let maybePromise = null;
+let currentKey = 0;
 
 // Configure a rotate api key
-async function fetch(url) {
+async function fetch(url, tryNextKey = keysCount, rotating = false) {
+  function rotateKeys(err) {
+    if (tryNextKey > 0) {
+      if (err.isAxiosError && err.response) {
+        if (err.response.status === 403) {
+          if (maybePromise === null || rotating) {
+            currentKey = (currentKey + 1) % keysCount;
+            maybePromise = fetch(url, tryNextKey - 1, true).finally(() => {
+              maybePromise = null;
+            });
+            return maybePromise;
+          } else {
+            return maybePromise.then(() => fetch(url, 0, false));
+          }
+        }
+      }
+    }
+
+    throw err;
+  }
+
+  api.defaults.headers.Authorization = `Bearer ${keys[currentKey]}`;
+
   const count = counterFetch.get();
-  setApiKeyHeader(api, count);
+  if (!rotating) {
+    const countStr = count.toString().padStart(6, " ");
+    logger.info(`[${countStr}]: ${url}`);
+  }
 
-  const countStr = count.toString().padStart(6, " ");
-  logger.info(`[${countStr}]: ${url}`);
-
-  const httpPromise = api.get(url).catch(catchSafeErrors);
-  const [data] = await Promise.all([httpPromise, sleep(500)]);
-  return data;
+  return await api.get(url).catch(rotateKeys).catch(catchSafeErrors);
 }
 
 module.exports = fetch;
